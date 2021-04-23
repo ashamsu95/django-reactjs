8ada5ec437bc6c2616e7248fa76c0587ab61a73a
ashamsu <ashamsuu@gmail.com>
2021-04-24 01:50:00 +0530

    created Post Serilizer and frontend react.js

---
+++ houseparty/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import Gird from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import TextField from "@material-ui/core/TextField";
+import FromControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import R
+
+
+export default class CreateRoomPage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <h1>hello hello World Room </h1>;
+    }
+}
---
+++ houseparty/frontend/src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import CreateRoomPage from "./CreateRoomPage";
+import RoomJoinPage from "./RoomJoinPage";
+import {BrowserRouter as Router,Switch,Route,Link,Redirect} from "react-router-dom"
+
+export default class HomePage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <Router>
+            <switch>
+                <Route exact path='/'><p>Loraitsum</p></Route>
+                <Route path='/join' component={RoomJoinPage}/>
+                <Route path='/create' component={CreateRoomPage} />
+            </switch>
+        </Router>;
+    }
+}
