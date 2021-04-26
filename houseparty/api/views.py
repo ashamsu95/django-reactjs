5b28d2df49345d717afadfc986d1b2119410c2b9
ashamsu <ashamsuu@gmail.com>
2021-04-26 18:27:23 +0530

    leave room integrated

--- houseparty/api/views.py
+++ houseparty/api/views.py
@@ -4,6 +4,7 @@ from .models import Room
 from .serializers import RoomSerializer,CreateRoomSerializer
 from rest_framework.views import APIView
 from rest_framework.views import Response
+from django.http import JsonResponse
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
@@ -78,3 +79,24 @@ class CreateRoomView(APIView):
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
 
 
+class UserInRoom (APIView):
+    def get(self,request,format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data={
+            'code':self.request.session.get('room_code')
+        }
+        return JsonResponse(data,status=status.HTTP_200_OK)
+
+class LeaveRoom(APIView):
+    def post(self,request,format=None):
+        if 'room_code' in self.request.session:
+           self.request.session.pop('room_code')
+           host_id = self.request.session.session_key
+           room_results = Room.objects.filter(host=host_id)
+           if len(room_results) > 0:
+               room = room_results[0]
+               room.delete()
+        
+        return Response({'message': 'Sucess'}, status=status.HTTP_200_OK)
